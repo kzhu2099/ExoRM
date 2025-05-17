9dd49d1ae27daf8c24c4534cfd30b4f127555c61
Kevin Zhu <kzhu2099@gmail.com>
2025-05-17 12:19:04 -0500

    improved

--- ExoRM/initialize_model.py
+++ ExoRM/initialize_model.py
@@ -1,39 +1,40 @@
-DEGREE = 2
-SMOOTHING = int(input('Enter smoothing amount (default 280): '))
+def initialize_model():
+    DEGREE = 2
+    SMOOTHING = int(input('Enter smoothing amount (default 280): '))
 
-import matplotlib.pyplot as plot
-import numpy
+    import matplotlib.pyplot as plot
+    import numpy
 
-from scipy.interpolate import UnivariateSpline
+    from scipy.interpolate import UnivariateSpline
 
-from ExoRM import ExoRM, unique_radius, read_rm_data, preprocess_data, ForecasterRM
+    from ExoRM import ExoRM, unique_radius, read_rm_data, preprocess_data, ForecasterRM
 
-data = read_rm_data()
-data = unique_radius(data)
-data = preprocess_data(data)
+    data = read_rm_data()
+    data = unique_radius(data)
+    data = preprocess_data(data)
 
-x = data['radius']
-y = data['mass']
+    x = data['radius']
+    y = data['mass']
 
-x = numpy.log10(x)
-y = numpy.log10(y)
+    x = numpy.log10(x)
+    y = numpy.log10(y)
 
-model = UnivariateSpline(x, y, k = DEGREE, s = SMOOTHING)
-model = ExoRM(model, x, y)
+    model = UnivariateSpline(x, y, k = DEGREE, s = SMOOTHING)
+    model = ExoRM(model, x, y)
 
-x_smooth = numpy.linspace(-0.5, 2, 10000)
-y_smooth = model(x_smooth)
+    x_smooth = numpy.linspace(-0.5, 2, 10000)
+    y_smooth = model(x_smooth)
 
-min_crossing = x_smooth[numpy.argmin(numpy.abs(y_smooth - ForecasterRM.terran(x_smooth)))]
-max_crossing = x_smooth[numpy.argmin(numpy.abs(y_smooth - ForecasterRM.stellar(x_smooth)))]
+    min_crossing = x_smooth[numpy.argmin(numpy.abs(y_smooth - ForecasterRM.terran(x_smooth)))]
+    max_crossing = x_smooth[numpy.argmin(numpy.abs(y_smooth - ForecasterRM.stellar(x_smooth)))]
 
-model.override_min(min_crossing, model(min_crossing))
-model.override_max(max_crossing, model(max_crossing))
+    model.override_min(min_crossing, model(min_crossing))
+    model.override_max(max_crossing, model(max_crossing))
 
-y_smooth = model(x_smooth)
+    y_smooth = model(x_smooth)
 
-plot.scatter(x, y, s = 0.3)
-plot.plot(x_smooth, y_smooth)
-plot.show()
+    plot.scatter(x, y, s = 0.3)
+    plot.plot(x_smooth, y_smooth)
+    plot.show()
 
-model.save('radius_mass_model.pkl')
+    model.save('radius_mass_model.pkl')
